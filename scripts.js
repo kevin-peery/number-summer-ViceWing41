0347c3f04a30da43667670372cf276559abfc63a
kevin-peery <kevin.peery@student.swic.edu>
2019-10-07 18:07:17 -0700

    Replace while with for loop.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
 // Write a program that sums the numbers from 1 to 10.
 
-let counter = 1;
 let sum = 0;
 
-// `<=` is  COMPARISON OPERATOR
-while (counter <= 10) {
-  sum += counter; // sum = sum + counter;
-
-  // `+=` is an ASSIGNMENT OPERATOR
-  counter += 1; // counter = counter + 1;
+//Loop
+for (let i = 1; i <= 10; i++) {
+  //Calculate
+  sum += i;
 }
-
+//Display
 console.log(sum);
