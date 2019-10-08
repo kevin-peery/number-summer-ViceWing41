572502bf5f3df442d4518dcc6e64702928301388
kevin-peery <kevin.peery@student.swic.edu>
2019-10-07 18:33:00 -0700

    Add function.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-// Write a program that sums the numbers from 1 to 10.
+//Declare function
+function numberSummer(start, end) {
+  console.log("fxn received arguments as follows: ", start, end);
+}
+
+//Invoke function
+numberSummer(1, 10);
 
 let sum = 0;
 
@@ -7,5 +13,6 @@ for (let i = 1; i <= 10; i++) {
   //Calculate
   sum += i;
 }
+
 //Display
 console.log(sum);
